1794d2b609bc8cd3196e92d7d71cda692e136134
PChana <superhipee@hotmail.com>
2020-02-17 15:06:09 +0700

    change to new database

--- php/config/database.php
+++ php/config/database.php
@@ -2,6 +2,6 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "ecom1";
+    $database = "cs402";
     $conn = new mysqli($servername, $username, $password, $database)or die("Connect failed: %s\n". $conn -> error);
 ?>
--- php/login.php
+++ php/login.php
@@ -5,12 +5,12 @@
         $username = $_POST['username'];
         $password = md5($_POST['password']);
 
-        $sql="SELECT * FROM user_info Where email='".$username."' and password='".$password."' ";
+        $sql="SELECT * FROM company Where company_user='".$username."' and company_password='".$password."' ";
         $result = mysqli_query($conn,$sql);
         if(mysqli_num_rows($result)==1){
             $row = mysqli_fetch_array($result);
             $_SESSION['u_id'] = $row['first_name'];
-            $_SESSION['fistname'] = $row['first_name'];
+            $_SESSION['name'] = $row['company_name'];
 
             echo 'success';
         }else{
