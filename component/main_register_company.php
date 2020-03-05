88a0c4397b1d1b90f04882a702eb1552ee645e60
pchanawut <superhipee@hotmail.com>
2020-03-05 16:18:04 +0700

    change component register.php

---
+++ component/main_register_company.php
@@ -0,0 +1,128 @@
+<!-- page's company -->
+<div class="container mt-5">
+    <form>
+        <div class="alert alert-primary" role="alert">
+            <h5>ข้อมูลของบริษัท</h5>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group col-8">
+                <label for="inputPassword4">ชื่อบริษัทหรือชื่อสถานที่ทำการ(ที่สามารถติดต่อได้):</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+            <div class="form-group col-4">
+                <label for="inputPassword4">จดทะเบียนวันที่:</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-3">
+                <label for="inputAddress">เลขทะเบียนบริษัท:</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="เลขทะเบียนบริษัท">
+            </div>
+            <div class="form-group col-4">
+                <label for="inputAddress">เบอร์โทรศัพท์:</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="เบอร์โทรศัพท์">
+            </div>
+            <div class="form-group col-5">
+                <label for="inputAddress">E-mail:</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="E-mail">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col">
+                <label for="inputAddress2">เลขที่:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">หมู่ที่:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">ถนน:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">ตำบล:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputState">อำเภอ:</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputState">จังหวัด:</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputZip">รหัสไปรษณีย์:</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+        </div>
+        <br>
+        <div class="alert alert-primary" role="alert">
+            <h5>ข้อมูลที่อยู่ในการจัดเก็บหรือใช้งานวัตถุ</h5>
+        </div>
+        <div class="form-row">
+            <div class="form-group col">
+                <label for="inputAddress2">ชื่อสถานที่จัดเก็บวัตถุ:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col">
+                <label for="inputAddress2">เลขที่:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">หมู่ที่:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">ถนน:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-group col-3">
+                <label for="inputAddress2">ตำบล:</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputState">อำเภอ:</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputState">จังหวัด:</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputZip">รหัสไปรษณีย์:</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+        </div>
+        <!-- <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                </div>
+            </div> -->
+        <button type="submit" class="btn btn-primary">ถัดไป</button>
+    </form>
+</div>
